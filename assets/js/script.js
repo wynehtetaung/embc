ff8df2ff9a2aed8c091f2e2cd6e5eabe544e076e
STLwin2003 <shinthantlwin70@gmail.com>
2024-01-30 22:51:24 +0630

    dark and light mode is completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,28 @@ english.addEventListener("click", () => {
   myanmar.classList.remove("hide");
   myanmar.classList.add("show");
 });
+
+var icon = document.getElementById("icon");
+let theme = String(localStorage.getItem("theme")).toLowerCase();
+localStorage.setItem("theme", theme);
+if (theme == "dark") {
+  document.body.classList.toggle("dark-theme");
+  icon.onclick;
+} else {
+  document.body.classList.remove("dark-theme");
+
+  icon.onclick;
+}
+
+icon.onclick = function () {
+  document.body.classList.toggle("dark-theme");
+  if (document.body.classList.contains("dark-theme")) {
+    localStorage.setItem("theme", "dark");
+    icon.src = "/assets/images/png/dark_mode.png";
+    obj.src = "/assets/images/png/OBJECTS2.png";
+  } else {
+    localStorage.setItem("theme", "light");
+    icon.src = "/assets/images/png/light_mode.png";
+    obj.src = "/assets/images/png/OBJECTS.png";
+  }
+};
