db04ab8672810ca9ae5d8ac69dfb29f418519dde
wynehtetaung <wynehtet2022@gmail.com>
2024-01-30 17:42:16 +0630

    localization switch button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,15 @@
-const embc = "hello embc";
+const myanmar = document.querySelector(".myan");
+const english = document.querySelector(".eng");
+
+myanmar.addEventListener("click", () => {
+  myanmar.classList.remove("show");
+  myanmar.classList.add("hide");
+  english.classList.remove("hide");
+  english.classList.add("show");
+});
+english.addEventListener("click", () => {
+  english.classList.remove("show");
+  english.classList.add("hide");
+  myanmar.classList.remove("hide");
+  myanmar.classList.add("show");
+});
