f452f578ab1f42ece4b11ba372dcf2530386af0c
wynehtetaung <wynehtet2022@gmail.com>
2024-01-31 06:59:01 +0630

    localization nav done

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@ const myanmar = document.querySelector(".myan");
 const english = document.querySelector(".eng");
 
 myanmar.addEventListener("click", () => {
+  localStorage.setItem("lang", "myanmar");
   myanmar.classList.remove("show");
   myanmar.classList.add("hide");
   english.classList.remove("hide");
   english.classList.add("show");
 });
 english.addEventListener("click", () => {
+  localStorage.setItem("lang", "english");
   english.classList.remove("show");
   english.classList.add("hide");
   myanmar.classList.remove("hide");
---
+++ assets/localization/nav.js
@@ -0,0 +1,46 @@
+const englishText = [
+  "HOME",
+  "PREDICT",
+  "METER",
+  "ITEM&UNITS",
+  "ABOUT",
+  "Myan",
+  "Eng",
+];
+const myanmarText = [
+  "ပင်မ",
+  "ခန့်မှန်း",
+  "မီတာ",
+  "ပစ္စည်းနှင့်ယူနစ်များ",
+  "အကြောင်း",
+  "မြန်မာ",
+  "အင်္ဂလိပ်",
+];
+
+const myanBtn = document.querySelector(".myan");
+const engBtn = document.querySelector(".eng");
+const textLang = document.querySelectorAll(".nav-lang");
+
+let getLang = String(localStorage.getItem("lang")).toLowerCase();
+localStorage.setItem("lang", getLang);
+
+if (getLang == "myanmar") {
+  for (let i = 0; i < textLang.length; i++) {
+    textLang[i].innerHTML = myanmarText[i];
+  }
+} else {
+  for (let i = 0; i < textLang.length; i++) {
+    textLang[i].innerHTML = englishText[i];
+  }
+}
+
+myanBtn.addEventListener("click", () => {
+  for (let i = 0; i < textLang.length; i++) {
+    textLang[i].innerHTML = myanmarText[i];
+  }
+});
+engBtn.addEventListener("click", () => {
+  for (let i = 0; i < textLang.length; i++) {
+    textLang[i].innerHTML = englishText[i];
+  }
+});
