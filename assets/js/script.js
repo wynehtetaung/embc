c466137c1bb7547029455fba25131d7ecc3d6eaf
wynehtetaung <wynehtet2022@gmail.com>
2024-02-01 09:15:39 +0630

    localization nav complete

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,25 @@
 const myanmar = document.querySelector(".myan");
 const english = document.querySelector(".eng");
-
-myanmar.addEventListener("click", () => {
-  localStorage.setItem("lang", "myanmar");
+function changeMyanmarBtn() {
   myanmar.classList.remove("show");
   myanmar.classList.add("hide");
   english.classList.remove("hide");
   english.classList.add("show");
-});
-english.addEventListener("click", () => {
-  localStorage.setItem("lang", "english");
+}
+function changeEnglishBtn() {
   english.classList.remove("show");
   english.classList.add("hide");
   myanmar.classList.remove("hide");
   myanmar.classList.add("show");
+}
+
+myanmar.addEventListener("click", () => {
+  localStorage.setItem("lang", "myanmar");
+  changeMyanmarBtn();
+});
+english.addEventListener("click", () => {
+  localStorage.setItem("lang", "english");
+  changeEnglishBtn();
 });
 
 var icon = document.getElementById("icon");
