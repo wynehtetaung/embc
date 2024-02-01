446ca847d7b89aa97ab40c360c903bff8212457c
STLwin2003 <shinthantlwin70@gmail.com>
2024-02-01 20:14:51 +0630

    detail pages are added

--- assets/js/predict.js
+++ assets/js/predict.js
@@ -3,43 +3,50 @@ items.forEach((item) =>
   item.addEventListener("click", (e) => {
     switch (e.target.classList[2]) {
       case "1":
-        location.href = "/assets/src/light-detail.html";
+        location.href = "/assets/src/detail/light-detail.html";
         break;
 
       case "2":
-        console.log("this is 2");
+        location.href = "/assets/src/detail/fan-detail.html";
         break;
       case "3":
-        console.log("this is 3");
+        location.href = "/assets/src/detail/computer-detail.html";
         break;
       case "4":
-        console.log("this is 4");
+        location.href = "/assets/src/detail/mobile-detail.html";
         break;
       case "5":
-        console.log("this is 5");
+        location.href = "/assets/src/detail/kitchen-detail.html";
         break;
       case "6":
-        console.log("this is 6");
+        location.href = "/assets/src/detail/air-con-detail.html";
+
         break;
       case "7":
-        console.log("this is 7");
+        location.href = "/assets/src/detail/W-mach-detail.html";
+
         break;
       case "8":
-        console.log("this is 8");
+        location.href = "/assets/src/detail/refri-detail.html";
+
         break;
       case "9":
-        console.log("this is 9");
+        location.href = "/assets/src/detail/tv-detail.html";
+
         break;
       case "10":
-        console.log("this is 10");
+        location.href = "/assets/src/detail/E-iron-detail.html";
+
         break;
 
       case "11":
-        console.log("this is 11");
+        location.href = "/assets/src/detail/general-detail.html";
+
         break;
 
       case "12":
-        console.log("this is 12");
+        location.href = "/assets/src/detail/W-con-detail.html";
+
         break;
     }
   })
